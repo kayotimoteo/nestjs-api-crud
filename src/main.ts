0e81dbe41150ca3e3a3e780f05a81791d9e7e026
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-09-12 15:06:42 -0400

    Após Configurações

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+
+import * as config from 'config'
+
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const serverConfig = config.get('server')
+  const logger = new Logger('bootstrap')
+  const app = await NestFactory.create(AppModule)
+
+  const port = process.env.PORT || serverConfig.port
+  await app.listen(port)
+  logger.log(`Application listering on port ${port}`)
+}
+bootstrap()
